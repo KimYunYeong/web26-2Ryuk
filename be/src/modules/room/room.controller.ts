fe88f6ecf8379189dd00c965af5adad379a63a1f
seha01130 <seha01130@gmail.com>
2026-01-13 17:44:25 +0900

    feat: http exception filter 추가

--- be/src/modules/room/room.controller.ts
+++ be/src/modules/room/room.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, HttpException, HttpStatus, Query } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { RoomService } from './room.service';
 import { RoomListResponseDto, RoomSearchQueryDto } from './dto/room.dto';
 import { ApiResponseMessage } from '@src/common/decorators/api-response-message.decorator';
@@ -11,36 +11,16 @@ export class RoomController {
   @Get('all')
   @ApiResponseMessage('방 목록 조회에 성공 했습니다.')
   async getLocalRooms(): Promise<RoomListResponseDto> {
-    try {
-      const rooms = await this.roomService.getLocalRooms();
-      return { rooms };
-    } catch (error) {
-      throw new HttpException(
-        {
-          success: false,
-          message: '서버 오류가 발생했습니다. 잠시 후 다시 시도해주세요.',
-        },
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
-    }
+    const rooms = await this.roomService.getLocalRooms();
+    return { rooms };
   }
 
   //로컬 방 검색 -> GET /api/rooms/search?keyword=검색어
   @Get('search')
   @ApiResponseMessage('방 검색 조회에 성공 했습니다.')
   async searchLocalRooms(@Query() query: RoomSearchQueryDto): Promise<RoomListResponseDto> {
-    try {
-      const keyword = query.keyword || '';
-      const rooms = await this.roomService.searchLocalRooms(keyword);
-      return { rooms };
-    } catch (error) {
-      throw new HttpException(
-        {
-          success: false,
-          message: '서버 오류가 발생했습니다. 잠시 후 다시 시도해주세요.',
-        },
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
-    }
+    const keyword = query.keyword || '';
+    const rooms = await this.roomService.searchLocalRooms(keyword);
+    return { rooms };
   }
 }
