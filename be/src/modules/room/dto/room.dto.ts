f30db6a728d9be20f9cc34385549d1bc41aa920f
seha01130 <seha01130@gmail.com>
2026-01-13 15:38:15 +0900

    feat: 로컬 방 목록 조회 응답 dto 추가

--- be/src/modules/room/dto/room.dto.ts
+++ be/src/modules/room/dto/room.dto.ts
@@ -11,3 +11,21 @@ export class RoomLeaveDto {
   @IsNotEmpty()
   roomId: string;
 }
+
+// 방 목록의 각각의 항목 dto
+export interface RoomListItemDto {
+  id: string;
+  title: string;
+  tags: string[];
+  current_participants: number;
+  max_participants: number;
+  is_mic_available: boolean;
+  is_private: boolean;
+  participant_profile_images: string[];
+  create_date: string;
+}
+
+// 방 목록 응답 시 사용하는 dto
+export interface RoomListResponseDto {
+  rooms: RoomListItemDto[];
+}
