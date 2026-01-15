a33d8eea3b782a4aec10dd798a4c83646a02d58b
seha01130 <seha01130@gmail.com>
2026-01-15 11:14:45 +0900

    feat: migration 설정 변경

--- be/src/app.module.ts
+++ be/src/app.module.ts
@@ -12,11 +12,27 @@ import { AuthModule } from './modules/auth/auth.module';
 import { RedisModule } from './providers/redis/redis.module';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+// Entities
+import { User } from './modules/user/user.entity';
+import { ChattingReport } from './modules/report/chatting-report.entity';
+import { Post } from './modules/post/post.entity';
+import { PostPicture } from './modules/post/post-picture.entity';
+import { PostLike } from './modules/post/post-like.entity';
+import { ChattingLog } from './modules/log/chatting-log.entity';
+import { Game } from './modules/game/game.entity';
+import { GameRecord } from './modules/game/game-record.entity';
+import { Comment } from './modules/comment/comment.entity';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot({ ...databaseConfig, entities: [], migrations: [] }),
+    TypeOrmModule.forRoot({
+      ...databaseConfig,
+      entities: [User, ChattingReport, Post, PostPicture, PostLike, ChattingLog, Game, GameRecord, Comment],
+      // 개발 환경에서는 마이그레이션 자동 실행 비활성화 (CLI로 별도 실행)
+      migrationsRun: false,
+      migrations: [],
+    }),
     RedisModule,
     ChatModule,
     RoomModule,
