55591418407823da32cea4d60ec90c9148afd337
seungjoonH <hsj6831@gmail.com>
2026-01-19 17:05:04 +0900

    fix: ci workflow 에 환경변수 추가

--- fe/next.config.mjs
+++ fe/next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
   async rewrites() {
     const apiServerUrl = process.env.NEXT_PUBLIC_API_SERVER_URL;
+    if (!apiServerUrl) throw new Error('환경변수가 없습니다: NEXT_PUBLIC_API_SERVER_URL');
     return [
       {
         source: '/api/:path*',
