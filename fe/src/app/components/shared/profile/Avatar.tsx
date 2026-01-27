e03023d56fc04c0d3e821ff21dab7d2ea6f07acb
seungjoonH <hsj6831@gmail.com>
2026-01-27 18:17:09 +0900

    refactor: 아바타 테마 인덱스 생성 로직 개선

--- fe/src/app/components/shared/profile/Avatar.tsx
+++ fe/src/app/components/shared/profile/Avatar.tsx
@@ -5,20 +5,24 @@ import Paths from '@/app/shared/path';
 import Image from 'next/image';
 import { AvatarProps } from './type';
 import CSSUtil from '@/utils/css';
+import { useCallback } from 'react';
 
 const DEFAULT_AVATAR = Paths.images('default_profile');
 const DEFAULT_THEMES = ['primary', 'secondary', 'success', 'warning'];
 
-function getThemeIndex(nickname?: string) {
-  if (!nickname) return 0;
-  let hash = 0;
-  for (let i = 0; i < nickname.length; i++) {
-    hash = (hash * 31 + nickname.charCodeAt(i)) >>> 0;
-  }
-  return hash % DEFAULT_THEMES.length;
-}
-
 export default function Avatar({ nickname, profileImage, isActive, onClick }: AvatarProps) {
+  const getThemeIndex = useCallback(
+    (nickname?: string) => {
+      if (!nickname) return 0;
+      let hash = 0;
+      for (let i = 0; i < nickname.length; i++) {
+        hash = (hash * 31 + nickname.charCodeAt(i)) >>> 0;
+      }
+      return (nickname.length * hash) % DEFAULT_THEMES.length;
+    },
+    [nickname],
+  );
+
   const index = getThemeIndex(nickname);
 
   const className = CSSUtil.buildCls(
