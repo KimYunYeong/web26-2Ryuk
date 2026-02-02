885a65b5c25868caec428cf91f6fc6ccfd65c8cc
seha01130 <seha01130@gmail.com>
2026-02-02 22:54:32 +0900

    refactor: game 도메인 리팩토링
    =- 책임 분리 수행

--- be/src/modules/game/game.module.ts
+++ be/src/modules/game/game.module.ts
@@ -6,11 +6,14 @@ import { GameController } from './game.controller';
 import { RoomModule } from '@src/modules/room/room.module';
 import { Game } from './game.entity';
 import { GameRecord } from '../game-record/game-record.entity';
+import { GameRedisService } from './game-redis.service';
+import { GameBroadcastService } from './game-broadcast.service';
+import { GameTimerService } from './game-timer.service';
 
 @Module({
   imports: [forwardRef(() => RoomModule), TypeOrmModule.forFeature([Game, GameRecord])],
   controllers: [GameController],
-  providers: [GameService, GameGateway],
+  providers: [GameService, GameGateway, GameRedisService, GameBroadcastService, GameTimerService],
   exports: [GameService],
 })
 export class GameModule {}
