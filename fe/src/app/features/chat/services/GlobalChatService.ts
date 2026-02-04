24f4874ae6645fe0834ef534a2c4312b230b56e7
seha01130 <seha01130@gmail.com>
2026-02-04 21:17:25 +0900

    fix: 전체 채팅 브로드캐스팅 렌더링 오류 해결

--- fe/src/app/features/chat/services/GlobalChatService.ts
+++ fe/src/app/features/chat/services/GlobalChatService.ts
@@ -54,6 +54,7 @@ export class GlobalChatService implements callback.ChatChannel {
     if (this.isSubscribed) return;
 
     this.isSubscribed = true;
+    this.attachHandlersToCurrentSocket();
 
     // 웹소켓 연결 후 초기 데이터 요청
     try {
