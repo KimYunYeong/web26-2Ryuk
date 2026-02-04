5b52f17410aad1ea2085e321045b81a17da47ff7
seha01130 <seha01130@gmail.com>
2026-02-05 01:13:02 +0900

    chore: 중복 호출 제거

--- fe/src/app/features/chat/services/GlobalChatService.ts
+++ fe/src/app/features/chat/services/GlobalChatService.ts
@@ -56,8 +56,6 @@ export class GlobalChatService implements callback.ChatChannel {
     this.isSubscribed = true;
     this.attachHandlersToCurrentSocket();
 
-    this.attachHandlersToCurrentSocket();
-
     // 웹소켓 연결 후 초기 데이터 요청
     try {
       const initDto = (await WebSocketService.request(
